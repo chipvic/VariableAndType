18edf8512bf00712d80dc88e38b512e15049b106
chip <huongctt3@onemount.com>
2021-08-08 22:08:56 +0700

    Homework 06/08/2021

--- src/main/java/BaiTH2.java
+++ src/main/java/BaiTH2.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class BaiTH2 extends HomeWork {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // Bài 1:
         System.out.println("Nhập số bất kỳ từ 1 đến 9");
-       int num = nhapSoInt();
-       readNum(num);
+        int num = nhapSoInt();
+        readNum(num);
 
-       // Bài 2:
+        // Bài 2:
         System.out.println("Nhập chữ cái bất kỳ");
         String text = nhapChuoi();
         lowerUpperChar(text);
@@ -40,23 +40,17 @@ public class BaiTH2 extends HomeWork {
         int c1 = nhapSoInt();
         int c2 = nhapSoInt();
         int c3 = nhapSoInt();
-        typeTriangle(c1,c2,c3);
-
-
-
-
-
-
+        typeTriangle(c1, c2, c3);
 
 
     }
 
-    static void readNum(int a){
-        switch (a){
+    static void readNum(int a) {
+        switch (a) {
             case 1:
                 System.out.println("Một");
                 break;
-                case 2:
+            case 2:
                 System.out.println("Hai");
                 break;
             case 3:
@@ -84,52 +78,54 @@ public class BaiTH2 extends HomeWork {
                 System.out.println("Số nằm ngoài từ 1 đến 9");
         }
     }
-    static void lowerUpperChar(String a){
+
+    static void lowerUpperChar(String a) {
         String tmp = a;
         String str = new String(tmp);
-        if (a.equals(str.toLowerCase())){
+        if (a.equals(str.toLowerCase())) {
             System.out.println(str.toUpperCase());
-        }
-        else {
+        } else {
             System.out.println(str.toLowerCase());
         }
     }
-    static void min (int a, int b, int c, int d){
-       int x = Math.min(a,b);
-       int y = Math.min(c,d);
-       int z = Math.min(x,y);
-     System.out.println("Min là: "+ z);
+
+    static void min(int a, int b, int c, int d) {
+        int x = Math.min(a, b);
+        int y = Math.min(c, d);
+        int z = Math.min(x, y);
+        System.out.println("Min là: " + z);
     }
-    static void receipt (int a){
-        double total =0;
-        if (a<=1){
+
+    static void receipt(int a) {
+        double total = 0;
+        if (a <= 1) {
             System.out.println("Tiền taxi là: 15000vnđ");
-        }
-        else{
-            if (a<=5){
-                total = ((a-1)*13500)+15000;
-                System.out.println("Tiền taxi là: "+total);
-            } else{
-                if(a<120){
-                    total =((a-5)*11000)+(4*13500)+15000;
-                    System.out.println("Tiền taxi là: "+total);
+        } else {
+            if (a <= 5) {
+                total = ((a - 1) * 13500) + 15000;
+                System.out.println("Tiền taxi là: " + total);
+            } else {
+                if (a < 120) {
+                    total = ((a - 5) * 11000) + (4 * 13500) + 15000;
+                    System.out.println("Tiền taxi là: " + total);
                 } else {
-                    total= (((a-5)*11000)+(4*13500)+15000)*0.9;
-                    System.out.println("Tiền taxi là: "+total);
+                    total = (((a - 5) * 11000) + (4 * 13500) + 15000) * 0.9;
+                    System.out.println("Tiền taxi là: " + total);
                 }
             }
         }
     }
-    static void days(int a, int b){
-        if(a==1||a==3||a==5||a==7||a==8||a==10||a==12){
+
+    static void days(int a, int b) {
+        if (a == 1 || a == 3 || a == 5 || a == 7 || a == 8 || a == 10 || a == 12) {
             System.out.println("Tháng có 31 ngày");
         } else {
-            if (a==4||a==6||a==9||a==11){
+            if (a == 4 || a == 6 || a == 9 || a == 11) {
                 System.out.println("Tháng có 30 ngày");
-            } else{
-                if(((b%100==0) && (b%400!=0))||(b%4!=0)){
+            } else {
+                if (((b % 100 == 0) && (b % 400 != 0)) || (b % 4 != 0)) {
                     System.out.println("Tháng có 28 ngày");
-                } else{
+                } else {
                     System.out.println("Tháng có 29 ngày");
                 }
 
@@ -137,18 +133,23 @@ public class BaiTH2 extends HomeWork {
         }
 
     }
-    static void typeTriangle (int a, int b, int c){
-        if ((a==b)&&(b==c)){
-            System.out.println("Tam giác đều");
-        } else{
-            if ((a==b)||(b==c)||(c==a)){
-                System.out.println("Tam giác cân");
-            } else{
-                if((a*a==b*b+c*c)||(b*b==a*a+c*c)||(c*c==a*a+b*b)){
-                    System.out.println("Tam giác vuông");
+
+    static void typeTriangle(int a, int b, int c) {
+        if ((a + b <= c) || (a + c <= b) || (b + c <= a)) {
+            System.out.println("Không phải tam giác");
+        } else {
+            if ((a == b) && (b == c)) {
+                System.out.println("Tam giác đều");
+            } else {
+                if ((a == b) || (b == c) || (c == a)) {
+                    System.out.println("Tam giác cân");
+                } else {
+                    if ((a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == a * a + b * b)) {
+                        System.out.println("Tam giác vuông");
+                    }
                 }
             }
         }
-    }
 
+    }
 }
