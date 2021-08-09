82a8addf00bd4def1dc5a289306ab703a4064ded
chip <huongctt3@onemount.com>
2021-08-09 07:52:41 +0700

    Homework 06/08/2021

--- src/main/java/BaiTH3.java
+++ src/main/java/BaiTH3.java
@@ -7,6 +7,15 @@ public class BaiTH3 extends HomeWork {
         leapYear(year);
 
         // Bài 2:
+        // Câu a:
+        System.out.println("Nhập ngày: ");
+        int d = nhapSoInt();
+        System.out.println("Nhập tháng: ");
+        int m = nhapSoInt();
+        System.out.println("Nhập năm: ");
+        int y = nhapSoInt();
+        checkDay(d,m,y);
+
 
 
 
@@ -19,4 +28,23 @@ public class BaiTH3 extends HomeWork {
             System.out.println("Năm không nhuận");
         }
     }
+
+    static void checkDay(int a, int b, int c){
+        if((a<=0)||(a>31)){
+            System.out.println("Ngày không hợp lệ");
+        } else{
+            if ((a>30)&&(b == 4 || b == 6 || b == 9 || b == 11)){
+                System.out.println("Ngày không hợp lệ");
+            } else{
+                if ((a==29)&&(b==2)&&(((c % 100 == 0) && (c % 400 != 0)) || (c % 4 != 0))){
+                    System.out.println("Ngày không hợp lệ");
+                } else {
+                    System.out.println("Ngày: "+a+" Tháng: "+b+ " Năm: " +c);
+                }
+
+            }
+        }
+    }
+
+
 }
